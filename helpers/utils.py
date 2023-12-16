45303fba9601ab5727b2b01e724bd92ac627c622
biniyam69 <biniyam.ajaw@astu.edu.et>
2023-12-16 21:01:03 +0300

    Did some EDA

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
 class Helpers:
@@ -9,4 +11,20 @@ class Helpers:
         df_normalized[columns] = scaler.fit_transform(df_normalized[columns])
         return df_normalized
 
-#%%
+    @staticmethod
+    def reduce(df):
+        numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
+        numerical_columns = numerical_columns.drop('MSISDN/Number', axis=1)
+        numerical_data = df[numerical_columns]
+        numerical_data = numerical_data.dropna()
+
+        scaler = StandardScaler()
+        numerical_data = scaler.fit_transform(numerical_data)
+
+        pca = PCA(n_components=2)
+        pca_result = pca.fit_transform(numerical_data)
+        pca_df = pd.DataFrame(data=pca_result, columns=['PC1', 'PC2'])
+        final_df = pd.concat([df, pca_df], axis=1)
+        return final_df
+
+
